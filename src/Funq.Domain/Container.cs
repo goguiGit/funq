f99a77f8fe5e63f7d3b9b38e8e0d308caa3a1cb5
Rodrigo Valdez (Gogui) <gogosms@gmail.com>
2025-02-18 17:13:15 +0100

    refactor, remove duplicates code

--- src/Funq.Domain/Container.cs
+++ src/Funq.Domain/Container.cs
@@ -5,42 +5,35 @@ public class Container
 {
     private readonly Dictionary<ServiceKey, object> _factories = new();
 
-    public void Register<TService>(Func<Container, TService> factory)
+    public void Register<TService>(Func<Container, TService> factory) 
+        => RegisterImpl<TService, Func<Container, TService>>(factory);
+
+    public void Register<TService, TArg>(Func<Container, TArg, TService> factory) 
+        => RegisterImpl<TService, Func<Container, TArg, TService>>(factory);
+
+    public void Register<TService, TArg, TArg2>(Func<Container, TArg, TArg2, TService> factory) 
+        => RegisterImpl<TService, Func<Container, TArg, TArg2, TService>>(factory);
+
+    public TService Resolve<TService>() 
+        => ResolveImpl<TService, Func<Container, TService>>(f => f(this));
+
+    public TService Resolve<TService, TArg>(TArg arg) 
+        => ResolveImpl<TService, Func<Container, TArg, TService>>(f => f(this, arg));
+
+    public TService Resolve<TService, TArg, TArg2>(TArg arg, TArg2 arg2) 
+        => ResolveImpl<TService, Func<Container, TArg, TArg2, TService>>(f => f(this, arg, arg2));
+
+    private void RegisterImpl<TService, TFunc>(TFunc factory)
     {
-        var key = new ServiceKey(typeof(TService), typeof(Func<Container, TService>));
-        _factories.Add(key, factory);
+        var key = new ServiceKey(typeof(TService), typeof(TFunc));
+        _factories.Add(key, factory!);
     }
 
-    public void Register<TService, TArg>(Func<Container, TArg, TService> factory)
+    private TService ResolveImpl<TService, TFunc>(Func<TFunc, TService> invoker)
     {
-        var key = new ServiceKey(typeof(TService), typeof(Func<Container, TArg, TService>));
-        _factories.Add(key, factory);
-    }
-
-    public void Register<TService, TArg, TArg2>(Func<Container, TArg, TArg2, TService> factory)
-    {
-        var key = new ServiceKey(typeof(TService), typeof(Func<Container, TArg, TArg2, TService>));
-        _factories.Add(key, factory);
-    }
-
-    public TService Resolve<TService>()
-    {
-        var key = new ServiceKey(typeof(TService), typeof(Func<Container, TService>));
-        var tService = _factories[key] as Func<Container, TService>;
-        return tService!.Invoke(this);
-    }
-
-    public TService Resolve<TService, TArg>(TArg arg)
-    {
-        var key = new ServiceKey(typeof(TService), typeof(Func<Container, TArg, TService>));
-        var tService = _factories[key] as Func<Container, TArg, TService>;
-        return tService!.Invoke(this, arg);
-    }
-
-    public TService Resolve<TService, TArg, TArg2>(TArg arg, TArg2 arg2)
-    {
-        var key = new ServiceKey(typeof(TService), typeof(Func<Container, TArg, TArg2, TService>));
-        var tService = _factories[key] as Func<Container, TArg, TArg2, TService>;
-        return tService!.Invoke(this, arg, arg2);
+        var key = new ServiceKey(typeof(TService), typeof(TFunc));
+        var tFunc = (TFunc)_factories[key];
+        var instance = invoker(tFunc);
+        return instance;
     }
 }
